5b1100caf468cd55d51f76e8ac0d7786cf8bd4b3
Frantic <sagittarius.00@mail.ru>
2024-11-27 20:10:10 +0300

    commit 1.1

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+import cgi, cgitb
+from django import forms
+def index(request):
+
+    return render(request, 'main/home.html')
+# Create your views here.
+def greeting(request):
+    name = request.GET.get('name')
+    message = request.GET.get('message')
+    return render(request, 'main/greeting.html', {'name':name,'message':message})
+
