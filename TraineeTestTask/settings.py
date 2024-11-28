30ff72a64d7274e13fb33225d4a564bc67e07ca2
Frantic <sagittarius.00@mail.ru>
2024-11-28 05:00:54 +0300

    commit 1.3 (Debug now is True added requirments.txt and allowed hosts)

--- TraineeTestTask/settings.py
+++ TraineeTestTask/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bh56l%!ru6&i-ey-)9nmo(zjjpeb00m3-7jaf3w@3((v%wlo3#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
