cc65c4cbb20733f963925fd8857dfcfba280ca27
Frantic <sagittarius.00@mail.ru>
2024-11-28 05:42:13 +0300

    commit 1.8

--- main/views.py
+++ main/views.py
@@ -5,8 +5,7 @@ import cgi, cgitb
 from django import forms
 def index(request):
 
-    #return render(request, 'home.html')
-    return HttpResponse("Hello everyone")
+    return render(request, 'home.html')
 # Create your views here.
 def greeting(request):
     name = request.GET.get('name')
